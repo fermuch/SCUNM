0f55c147a4f69d2174961c2ebd8eb36b2c0376ab
Fernando Mumbach (fermuch) <fermuch@gmail.com>
2017-10-15 16:54:49 -0200

    using bot token from env var

--- Application/app.js
+++ Application/app.js
@@ -5,5 +5,5 @@ var gamesAssets = require("../ReinoDelMal");//load games assets
 var Engine = require("../SCUNMEngine");
 var demoEngine = new Engine(gamesAssets.Demo);//create a engine for every game asset
 var SCUNMBot = require("../SCUNMBot");
-var botTokenAuth = "411814980:AAEBIgh3zpxqA97xGJUIHYa5N9B3uXPCbas";
+var botTokenAuth = process.env.BOT_TOKEN;
 var myGameBot = new SCUNMBot(botTokenAuth, { polling: true }, demoEngine, store);//create bot for demo engine
